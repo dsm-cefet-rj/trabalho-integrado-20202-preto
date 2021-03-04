2b19fec091a1ecb90a2cfc8264dd970aa7afcfb6
Isaac <ribeiroo.isaac@gmail.com>
2021-03-04 16:37:27 -0300

    Finalizando tela de Criação de Anúncio e estilizando botão

--- paw-friends/src/components/AnimalCreateForm.js
+++ paw-friends/src/components/AnimalCreateForm.js
@@ -1,7 +1,6 @@
 // Componente que recebe um título e renderiza numa linha da página
 import React from 'react';
 
-
 function AnimalCreateForm() {
     const controleAnuncio = event =>{
       event.preventDefault();
@@ -9,16 +8,16 @@ function AnimalCreateForm() {
     }
 
     return (
-        <div className="formulario">
+        <div className="animalcreateform">
           <form onSubmit={controleAnuncio}>
           <fieldset>
               <label>
 
-                  <p>Nome do animal</p>
+                  <p>Nome do animal  <br/>
                   <input nome="nome"/>
-                  <br/>
+                  </p>
 
-                  <p>Tipo</p>
+                  <p>Tipo <br/>
                       <select name="tipo">
                       <option value="">--Escolha o correspondente--</option>
                       <option value="ave">Ave</option>
@@ -28,43 +27,135 @@ function AnimalCreateForm() {
                       <option value="gato">Gato</option>
                       <option value="hamster">Hamster</option>
                       </select>
-                  <p>Raça</p>
+                  </p>
+
+                  <p>Raça<br/>
                   <input raça="raça"/>
-                  <br/>
-                  <p>Idade</p>
+                  </p>
+
+                  <p>Idade<br/>
                   <input idade="idade"/>
-                  <br/>
-                  <p>Sexo</p>
-                  <input sexo="s"/>
-                  <br/>
-                  <p>Temperamento</p>
+                  </p>
+
+                  <p>Sexo<br/>
+                      <select name="sexo">
+                      <option value="">--Escolha o sexo do animal--</option>
+                      <option value="masculino">Macho</option>
+                      <option value="feminino">Fêmea</option>
+                      </select>
+                  </p>
+
+                  <p>Temperamento<br/>
                       <select name="temperamento">
-                      <option value="">--Escolha o correspondente--</option>
+                      <option value="">--Escolha o temperamento--</option>
                       <option value="manso">Manso</option>
                       <option value="neutro">Neutro</option>
                       <option value="agressivo">Agressivo</option>
                       </select>
-                  <p>Estado</p>
+                  </p>
+
+                  <p>Estado<br/>
                   <input estado="estado"/>
-                  <br/>
-                  <p>Cidade</p>
+                  </p>
+
+                  <p>Cidade<br/>
                   <input cidade="cidade"/>
-                  <br/>
-                  <p>Bairro</p>
+                  </p>
+
+                  <p>Bairro<br/>
                   <input bairro="bairro"/>
-                  <br/>
-                  <p>Descrição</p>
-                  <input descrição="descrição"/>
-                  <br/>
-                  <p>Foto</p>
+                  </p>
+
+                  <p>Descrição<br/>
+                  <textarea descrição="descrição"  
+                  cols={48}
+                  rows={6}
+                  />
+                  </p>
+
+                  <p>Foto<br/>
                   <input type="file" name="foto"/>
+                  </p>
               </label>
           </fieldset>
-          <br/>
-          <button type="submit">Criar anúncio</button>
+          <button class="btn btn-outline-dark mt-3 text-capitalize" type="submit">Criar anúncio</button>
           </form>
         </div>
       )
 }
 
 export default AnimalCreateForm;
+
+
+
+/*
+<Form>
+  <Form.Group controlId="exampleForm.ControlInput1">
+    <Form.Label>Nome</Form.Label>
+    <Form.Control type="nome" placeholder="Nome do Animal" />
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Label>Tipo</Form.Label>
+    <Form.Control as="select">
+      <option value='cachorro'>Cachorro</option>
+      <option value='calopsita'>Calopsita</option>
+      <option value='coelho'>Coelho</option>
+      <option value='cobra'>Cobra</option>
+      <option value='gato'>Gato</option>
+      <option value='hamster'>Hamster</option>
+    </Form.Control>
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlInput1">
+    <Form.Label>Raça</Form.Label>
+    <Form.Control type="raça" placeholder="Raça do Animal" />
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlInput1">
+    <Form.Label>Idade</Form.Label>
+    <Form.Control type="number" placeholder="Idade do animal" />
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlSelect2">
+    <Form.Label>Sexo</Form.Label>
+    <Form.Control as="select" multiple>
+      <option value='masculino'>Macho</option>
+      <option value='feminino'>Fêmea</option>
+    </Form.Control>
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlSelect2">
+    <Form.Label>Temperamento</Form.Label>
+    <Form.Control as="select" multiple>
+      <option value='agressivo'>Agressivo</option>
+      <option value='neutro'>Neutro</option>
+      <option value='manso'>Manso</option>
+    </Form.Control>
+  </Form.Group>
+
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>Estado</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>Cidade</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>Bairro</Form.Label>
+      <Form.Control />
+    </Form.Group>
+  </Form.Row>  
+
+  <Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Descrição</Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+  <Form.Group>
+    <Form.File id="exampleFormControlFile1" label="Foto do Animal" />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">Criar Anúncio</Button>
+
+</Form>
+
+*/
