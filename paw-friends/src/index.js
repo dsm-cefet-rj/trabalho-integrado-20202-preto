efc28603d1a32392fda97d51b1610408a997dbee
Isaac <ribeiroo.isaac@gmail.com>
2021-03-04 17:19:58 -0300

    dicionando tela de edição de anúncio

--- paw-friends/src/index.js
+++ paw-friends/src/index.js
@@ -4,10 +4,11 @@ import './app.css';
 import IndexPage from './components/IndexPage';
 import AnimalPage from './components/AnimalPage';
 import AnimalCreatePage from './components/AnimalCreatePage';
+import AnimalEditPage from './components/AnimalEditPage';
 
 //Renderiza o componente IndexPage.js em uma div no index.html
 //Esse componente se refere ao conjunto de componentes que constituem a página
 ReactDOM.render(
-  <AnimalCreatePage />,
+  <AnimalEditPage />,
   document.getElementById('IndexPageComponent')
 );
