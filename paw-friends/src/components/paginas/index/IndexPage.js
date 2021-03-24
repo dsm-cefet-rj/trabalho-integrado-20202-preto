610a0a554d7d89b9131788f23d9f3df67cc86c63
fshanom <filipeshanom@gmail.com>
2021-03-24 14:30:50 -0300

    Fetch status handler na página index

--- paw-friends/src/components/paginas/index/IndexPage.js
+++ paw-friends/src/components/paginas/index/IndexPage.js
@@ -5,15 +5,31 @@ import { useSelector } from 'react-redux'
 
 export default function IndexPage() {
     const anuncios = useSelector(state => state.anuncios);
+    const status = anuncios.status;
+    const error = anuncios.error;
+    var pageTitle = '';
+    let anunciosRow = null;
+
+    if(status === 'loaded'){
+        pageTitle = 'Animais para adoção';
+        anunciosRow = anuncios.anunciosObjs.map((anuncio) => {
+            return <Card key={anuncio.key} anuncio={anuncio}/>;
+        });
+    }
+    else if(status == 'loading'){
+        pageTitle = 'Carregando anúncios';
+    }
+    else if(status == 'failed'){
+        pageTitle = 'Erro! ' + error;
+    }
+
     return (
         <>
             <Navbar />
-            <PageTitle title={"Animais para adoção"} />
+            <PageTitle title={pageTitle} />
             <div className="container-fluid container-cards">
                 <div className="row d-flex justify-content-center mb-5">
-                    {anuncios.anunciosObjs.map((anuncio) => {
-                        return <Card key={anuncio.key} anuncio={anuncio}/>;
-                    })}
+                    {anunciosRow}
                 </div>
             </div>
         </>
