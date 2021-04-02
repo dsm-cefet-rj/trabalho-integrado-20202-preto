9526bd73c418ebfd27a00f1b920d3571830dceeb
BFerrari <brunoferrarisenhora@gmail.com>
2021-04-01 22:23:15 -0300

    layout do chat terminado

--- paw-friends/src/components/paginas/chat/PrivateChatPage.js
+++ paw-friends/src/components/paginas/chat/PrivateChatPage.js
@@ -5,21 +5,17 @@ function PrivateChatPage () {
     return (
         <>
             <Navbar/>
-            <div className="container-fluid">
-                <div className="card card-chat" >
+            <div className="container-fluid privatechat">
+                <div className="container-fluid headerchat fixed">
                     <div className="row">
-                        <div className="col-6 col-sm-6">
-                            <img className="card-img-chatpicperson" src="https://sunrift.com/wp-content/uploads/2014/12/Blake-profile-photo-square.jpg" alt="Cardcap"/>
-                            <img className="card-img-chatpicanimal" src="https://shortyawards.imgix.net/entries/12th/2c77445c-4920-447a-8c8f-6f4a2d89ed3e.jpeg?auto=format&fit=crop&h=400&q=65&w=400&s=85162f99d736e3eb6e30a6aca8af8bee" alt=""/>
-                        </div>
-                    
-                        <div className="col-6 col-sm-6">
-                            <div className="card-body">
-                                <h5 className="card-title">Jason Murry</h5>
-                                <h6 className="card-title">Sr.Bigodes</h6>
-                            </div>
-                        </div>
-                    </div>                   
+                            <img alt="" className="col-xs-2 col-md-3 avatarprivate" src="https://i.pinimg.com/originals/9e/21/86/9e2186e732c8c717a0d3c74a22a4d8ff.jpg" />
+                            <div className="col-md-4 animaltitle"> Jason Momoa</div>
+                    </div>
+                </div>
+                <div className="container-fluid mesagebox fixed-bottom">
+                    <div className="row chatboxmesage">
+                        <input className="form-control" type="text" placeholder="type your mesage" />
+                    </div>
                 </div>
             </div>
         </>
