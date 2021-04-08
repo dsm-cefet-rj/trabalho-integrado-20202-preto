0880de745f85139e84e11d440fac1446afc1cf65
BFerrari <brunoferrarisenhora@gmail.com>
2021-04-08 17:51:44 -0300

    Corrigido bug no chat e adicionado estados na página do chat.

--- paw-friends/src/components/paginas/chat/PrivateChatPage.js
+++ paw-friends/src/components/paginas/chat/PrivateChatPage.js
@@ -1,13 +1,39 @@
 import Navbar from '../../layouts/navbar/Navbar';
-import React from 'react';
+import React, { useState } from 'react';
 import PrivateChatCards from './PrivateChatCards';
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAnuncios, selectAnunciosById } from '../../../store/reducers/anunciosReducer';
+import { useLayoutEffect } from 'react';
 
 export default function PrivateChatPage () {
-    
+
+    const status = useSelector(state => state.anuncios.status);
+    const dispatch = useDispatch();
+    let { id } = useParams();
+    id = parseInt(id);
+
+    useLayoutEffect(() => {
+        if(status === 'not_loaded'){
+            dispatch(fetchAnuncios());
+        }
+    }, [status, dispatch]);
+
+    let anuncio = useSelector(state => selectAnunciosById(state, id));
+
+//segura o componente vazio até o estado mudar
+if (anuncio === undefined) {
+    anuncio = {};
+}   
+
+    const [chats, setChats] = useState([
+        "Diga Oi e comece sua adoção!"
+    ]); 
+
     return (
         <>
             <Navbar/>
-            <PrivateChatCards/>
+            <PrivateChatCards chats={chats} setChats={setChats} anuncio={anuncio}/>
         </>
     );
 }
