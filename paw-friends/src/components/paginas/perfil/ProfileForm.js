5d7422fe2b3bbfa47eb7855acc4850840608bfd6
Isaac <ribeiroo.isaac@gmail.com>
2021-04-14 23:23:37 -0300

    Validação de formulário de perfil

--- paw-friends/src/components/paginas/perfil/ProfileForm.js
+++ paw-friends/src/components/paginas/perfil/ProfileForm.js
@@ -3,10 +3,11 @@ import React, {useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {Link, useHistory, useParams} from 'react-router-dom';
 import {addProfileServer, updateProfileServer, selectProfilesById} from '../../../store/reducers/profilesReducer'
+import { profileSchema } from './profileSchema';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from "react-hook-form";
 
 // import useForm from 'react-hook-form'; //
-
 
 function ProfileForm(props) {
 
@@ -18,8 +19,12 @@ function ProfileForm(props) {
 
     const profileById = useSelector(state => selectProfilesById(state, id))
 
-    const [profile, setProfile] = useState(
-        id ? profileById ?? {} : {});
+    const { register, handleSubmit, errors } = useForm({
+        resolver:yupResolver(profileSchema)
+    });
+
+    const [profileOnLoad] = useState(
+        id ? profileById ?? profileSchema.cast({}): profileSchema.cast({}));
 
     const [actionType, ] = useState(
         id ? profileById
@@ -27,17 +32,12 @@ function ProfileForm(props) {
             : 'profiles/addProfile'
             : 'profiles/addProfile');
 
-    function handleInputChange(e){
-        setProfile ({...profile, [e.target.name]: e.target.value})            
-    }
-
-    function PerfilCriado(e){
-        e.preventDefault();
+    function onSubmit(profile){
         if(actionType === 'profiles/addProfile'){
             dispatch(addProfileServer(profile));
             alert("Você criou sua conta!")
         }else{
-            dispatch(updateProfileServer(profile));
+            dispatch(updateProfileServer({...profile, id: profileById.id}));
             alert("Você editou sua conta!")
         }
         history.push(`/perfil/${id}`);
@@ -55,59 +55,69 @@ function ProfileForm(props) {
             <div className="col-lg-4 col-md-8 col-sm-8 col-10">
                 <div class="card">
                     <div class="card-body">
-                        <form onSubmit={PerfilCriado}>
+                        <form onSubmit={handleSubmit(onSubmit)}>
                             <div class="form-group">
                                 <label for="InputName">Nome</label>
-                                <input type="text" name="nome" value={profile.Nome} onChange={handleInputChange} class="form-control" placeholder="Nome"/>
+                                <input type="text" name="nome" defaultValue={profileOnLoad.Nome} ref={register} class="form-control" placeholder="Nome"/>
+                                <div style={{color: "red"}}>{errors.Nome?.message}</div>
                             </div>
                             <div class="form-group mt-2">
                                 <label for="InputName">Sobrenome</label>
-                                <input type="text" name="sobrenome" value={profile.Sobrenome} onChange={handleInputChange} class="form-control" placeholder="Sobrenome"/>
+                                <input type="text" name="sobrenome" defaultValue={profileOnLoad.Sobrenome} ref={register} class="form-control" placeholder="Sobrenome"/>
+                                <div style={{color: "red"}}>{errors.Sobrenome?.message}</div>
                             </div>
         
                             <div class="form-group mt-2">
                                 <label for="InputCPF/CNPJ">CPF ou CNPJ</label>
-                                <input type="number" name="cpf" value={profile.CPF} onChange={handleInputChange} class="form-control" placeholder="CPF ou CNPJ"/>
+                                <input type="number" name="cpf" defaultValue={profileOnLoad.CPF} ref={register} class="form-control" placeholder="CPF ou CNPJ"/>
+                                <div style={{color: "red"}}>{errors.CPF?.message}</div>
                             </div>
 
                             <div class="form-group mt-2">
                                 <label for="InputCity">Cidade</label>
-                                <input type="text" name="cidade" value={profile.Cidade} onChange={handleInputChange} class="form-control" placeholder="Cidade"/>
+                                <input type="text" name="cidade" defaultValue={profileOnLoad.Cidade} ref={register} class="form-control" placeholder="Cidade"/>
+                                <div style={{color: "red"}}>{errors.Cidade?.message}</div>
                             </div>
 
                             <div class="form-group mt-2">
                                 <label for="InputCEP">CEP</label>
-                                <input type="number" name="cep" value={profile.CEP} onChange={handleInputChange} class="form-control" placeholder="Insira o CEP"/>
+                                <input type="number" name="cep" defaultValue={profileOnLoad.CEP} ref={register} class="form-control" placeholder="Insira o CEP"/>
+                                <div style={{color: "red"}}>{errors.CEP?.message}</div>
                             </div>
 
                             <div class="form-group mt-2">
                                 <label for="InputPhone">Telefone</label>
-                                <input type="tel" name="telefone" value={profile.Telefone} onChange={handleInputChange} class="form-control" placeholder="Número de Telefone"/>
+                                <input type="tel" name="telefone" defaultValue={profileOnLoad.Telefone} ref={register} class="form-control" placeholder="Número de Telefone"/>
+                                <div style={{color: "red"}}>{errors.Telefone?.message}</div>
                             </div>
 
                             <div class="form-group mt-2">
                                 <label for="InputAge">Email</label>
-                                <input type="email" name="email" value={profile.Email} onChange={handleInputChange} class="form-control" placeholder="Insira seu Email"/>
+                                <input type="email" name="email" defaultValue={profileOnLoad.Email} ref={register} class="form-control" placeholder="Insira seu Email"/>
+                                <div style={{color: "red"}}>{errors.Email?.message}</div>
                             </div>
 
                             <div class="form-group mt-2">
                                 <label for="tipoAnimal">Nome de Usuário</label>
-                                <input type="text" name="user" value={profile.User} onChange={handleInputChange} class="form-control" placeholder="Nome de Usuario" />
+                                <input type="text" name="user" defaultValue={profileOnLoad.User} ref={register} class="form-control" placeholder="Nome de Usuario" />
+                                <div style={{color: "red"}}>{errors.User?.message}</div>
                             </div>
 
                             <div class="form-group mt-2">
                                 <label for="InputPass">Senha</label>
-                                <input type="password" name="senha" value={profile.Senha} onChange={handleInputChange} class="form-control" placeholder="Senha"/>
+                                <input type="password" name="senha" defaultValue={profileOnLoad.Senha} ref={register} class="form-control" placeholder="Senha"/>
+                                <div style={{color: "red"}}>{errors.Senha?.message}</div>
                             </div>
 
                             <div class="form-group mt-2">
                                 <label for="InputPassConfirmation">Confirme a Senha</label>
-                                <input type="password" name="senha" value={profile.Senha} onChange={handleInputChange} class="form-control" placeholder="Senha"/>
+                                <input type="password" name="senha" defaultValue={profileOnLoad.Senha} ref={register} class="form-control" placeholder="Senha"/>
+                                <div style={{color: "red"}}>{errors.Senha?.message}</div>
                             </div>
 
                             <div class="image-file mt-2">
                                 <label class="custom-file-label">Foto</label>
-                                <input type="file" value={profile.Img} onChange={handleInputChange} class="custom-file-input"/> 
+                                <input type="file" defaultValue={profileOnLoad.Img} ref={register} class="custom-file-input"/> 
                                 <div class="invalid-feedback">Arquivo não válido</div>
                             </div>
 
